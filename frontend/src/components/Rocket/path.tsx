d5a9b908fd0efd3895ddda892c55c07e7022e1dd
Namra Vamja <namravamja00@gmail.com>
2025-09-30 11:32:22 +0530

    refactor: Simplify RocketEffect component and update SVG path
    - Removed unnecessary complexity from RocketEffect component.
    - Integrated video background for enhanced visual appeal.
    - Updated SVG path for improved rocket trajectory representation.
    - Streamlined imports and component structure for better readability.

--- frontend/src/components/Rocket/path.tsx
+++ frontend/src/components/Rocket/path.tsx
@@ -3,19 +3,10 @@ import React from "react";
 const path = () => {
   return (
     <div>
-      <svg
-        width="2200"
-        height="120"
-        viewBox="0 0 1773 98"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 96.2261C1353.82 96.2261 1154.74 96.226 1243.45 96.226C1368.79 96.226 1789.5 111 1771 1"
-          stroke="black"
-          strokeWidth="4"
-        />
-      </svg>
+      <svg width="1011" height="531" viewBox="0 0 1011 531" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 522.5C1 522.5 207.067 539.112 337.375 522.5C457.169 507.229 528.822 500.658 637.5 448C741.459 397.629 798.677 357.436 873 269C948.639 178.998 1010.12 1 1010.12 1" stroke="black"/>
+</svg>
+
     </div>
   );
 };
